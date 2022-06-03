6ffac26568d6d3c538784fe1c792318484bda25a
RafaelLeone <rinaciodacosta@gmail.com>
2022-06-03 08:54:45 -0300

    03-14 feito

--- secao_03_estrutura_de_repeticao/ex_14_qtde_pares_e_impares.py
+++ secao_03_estrutura_de_repeticao/ex_14_qtde_pares_e_impares.py
@@ -18,3 +18,14 @@ Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade 
 
 def calcular_qtde_numeros_pares_e_impares(n1: int, n2: int, n3: int, n4: int, n5: int, n6: int, n7: int, n8: int, n9: int, n10: int) -> str:
     """Escreva aqui em baixo a sua solução"""
+    par = 0
+    imp = 0
+    i = 0
+    lista = [n1, n2, n3, n4, n5, n6, n6, n7, n8, n9, n10]
+    for i in range(len(lista)-1):
+        if lista[i] % 2 == 0:
+            par += 1
+        else:
+            imp += 1
+        i += 0
+    print(f"'Existem {par} números pares e {imp} números impares'")
