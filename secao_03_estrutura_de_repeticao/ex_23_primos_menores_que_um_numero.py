fa03162568daaacd11820af459ea29ca76eb0bc4
RafaelLeone <rinaciodacosta@gmail.com>
2022-06-06 10:38:53 -0300

    secao 03 pronta

--- secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
+++ secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
@@ -38,6 +38,7 @@ Serão avaliados o funcionamento, o estilo e o número de testes (divisões) exe
     True
 
 """
+from ntpath import join
 from typing import Tuple
 
 
@@ -45,19 +46,28 @@ def calcular_primos_e_divisoes(n: int) -> Tuple[str, int]:
     """Escreva aqui em baixo a sua solução"""
     if n <= 1:
         return '', 0
+    x = n
     lista = []
     lista2 = []
     i = n-1
+    ndiv = 0
+    contador = 1
     while n > 1:
         while i > 1:
             lista.append(n%i)
             i -= 1
         if 0 not in lista:
-            lista2.append(n)
+            lista2.append(str(n))
+            ndiv += 1*contador
+            contador += 1
         n -= 1
         i = n-1
         lista = []
     lista2.reverse()
-    print(lista2)
-calcular_primos_e_divisoes(2) 
+    if x == 3:
+        ndiv = 1
+    if x <= 2:
+        ndiv = 0
+    y = ", ".join(lista2)
+    return y, ndiv
 
