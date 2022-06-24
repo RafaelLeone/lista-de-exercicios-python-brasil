28cc5475ced35903de8f1b045c6cc8eda4f8506d
RafaelLeone <rinaciodacosta@gmail.com>
2022-06-24 16:16:03 -0300

    feito 05-01 mas sem enunciado ainda

--- secao_05_exercicios_funcoes/ex_01_piramide.py
+++ secao_05_exercicios_funcoes/ex_01_piramide.py
@@ -13,3 +13,10 @@ Faça um programa para imprimir:
 para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
 
 """
+
+
+def imprimir_piramide(n: int):
+  for i in range(1, n + 1):
+    for _ in range(i):
+      print(i, end='   ')
+    print('')
