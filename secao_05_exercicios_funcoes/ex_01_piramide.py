de53b1e11f99b3abba777e67fe87f51dc402fb3b
RafaelLeone <rinaciodacosta@gmail.com>
2022-06-24 16:20:39 -0300

    chega por hoje

--- secao_05_exercicios_funcoes/ex_01_piramide.py
+++ secao_05_exercicios_funcoes/ex_01_piramide.py
@@ -20,3 +20,10 @@ def imprimir_piramide(n: int):
     for _ in range(i):
       print(i, end='   ')
     print('')
+
+#resolução ex2:
+# def imprimir_piramide_crescente(n: int):
+#   for linha in range(1, n + 1):
+#     for coluna in range(1, linha + 1):
+#       print(coluna, end='   ')
+#     print('')
